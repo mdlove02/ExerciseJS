d6d907e624f8d6bd99a336870c8c91a1b0a1282d
Matt Love <matthew.love683@gmail.com>
2021-03-13 21:26:16 -0500

    reformat done

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,59 @@
-/*Form*/
+/*validating form*/
 
 function validate() {
-  let name = document.getElementById("name").value;
+  
+  let fullName = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let number = document.getElementById("phoneNumber").value;
   let password = document.getElementById("password").value;
-  let error = document.getElementById("error"); 
-  let nameRegex = /^[a-z ,.'-]+$/i;
-  let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  let phoneRegex = /[^\s-]?\(?(\d{3})\)?[\s-]?\d{3}[\s-]?\d{4}/g;
-  let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
-  
+  let error = document.getElementById("error");
   let message = ["Full name must be used",
   "Valid email must be used", 
   "Phone number must include area code", 
   "Password must contain ≥ 8 characters, 1 uppercase and 1 digit"];
 
-  if(name != name.match(nameRegex)) {
+  if(fullName != fullName.match(/^[a-z ,.'-]+$/i)) {
     error.innerHTML = message[0];
     return false;
-  } else if(email != email.match(emailRegex)) {
+  } 
+  if(email != email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
     error.innerHTML = message[1];
     return false;
-  } else if(number != number.match(phoneRegex)) {
+  } 
+  if(number != number.match(/[^\s-]?\(?(\d{3})\)?[\s-]?\d{3}[\s-]?\d{4}/g)) {
     error.innerHTML = message[2];
     return false;
-  } else if(password != password.match(passwordRegex)) {
+  } 
+  if(password != password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
     error.innerHTML = message[3];
     return false;
   } else {
-    alert ("Thanks for all your information" + " " + name);
+    alert ("Thanks for all your information" + " " + fullName);
     return true;
   } 
   
-};
+}
 
+/*reformating phone number*/
 
+function reformat() {
+  let number = document.getElementById("phoneNumber");
+  number.addEventListener("keyup", function(event) {
+      let n = this.value.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+      number.value = n;
+  });
 
+}
 
- 
+/*Sending form*/
+
+function savePersonalData(){
+  $.ajax({
+      url: "https://api.apispreadsheets.com/data/9259/",
+      type:'post',
+      data:$("newForm").serializeArray()
+  });
+
+}
+
 
