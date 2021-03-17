1f3646dd607555d2be74b9ebf34940b09cce4de4
Matt Love <matthew.love683@gmail.com>
2021-03-17 02:51:57 -0400

    posting to excel

--- scripts/equations.js
+++ scripts/equations.js
@@ -1,26 +1,40 @@
 /*Calculation*/
 
-function changeWeight() {
-    let inputAge = document.getElementById("age");
-    let inputHt = document.getElementById("height");
-    let inputWt = document.getElementById("weight");
-    let inputHr = document.getElementById("hr");
+let inputAge = document.getElementById("age");
+let inputHt = document.getElementById("height");
+let inputWt = document.getElementById("weight");
+let inputResult = document.getElementById("results");
     
   
-     //BMI
-    if(inputTypeWT.value == "lbs" && inputTypeHt.value == "in") {  
-      result.value = (inputWt.value)/[inputHt.value * inputHt.value]*703;
-    }  
+//BMI
+function bmi() {
+  let inputResult = document.getElementById("results");
+  inputResult.addEventListener("click", function() {
+    let bmi = inputWT.value / [(inputWt.value**2)]*703;
+    inputResult.value = bmi;
+    if(bmi < 18.5){
+      return "Underweight";
+    }else if (bmi < 25){
+      return "Normal";
+    }else if (bmi < 30){
+      return "Overweight";
+    }else{
+      return "Obese";
+    }
+  });
+  
+
+      
   
     //RMR Male
-    if(inputTypeWT.value == ("lbs")/2.2 && inputTypeHt.value == ("in")*2.54) {
+    if(inputWT.value == ("lbs")/2.2 && inputHt.value == ("in")*2.54) {
       result.value = ((inputWT.value)*10)+((inputHt.value)*6.25)-((inputAge.value)*5)+5;
     } 
   
     //RMR Female
-    if(inputTypeWT.value == ("lbs")/2.2 && inputTypeHt.value == ("in")*2.54) {
+    if(inputWT.value == ("lbs")/2.2 && inputHt.value == ("in")*2.54) {
       result.value = ((inputWT.value)*10)+((inputHt.value)*6.25)-((inputAge.value)*5)-161;
     }
   
    
-  }
+ 
--- scripts/index.js
+++ scripts/index.js
@@ -1,59 +1,58 @@
-/*validating form*/
+/*Sending form*/
 
-function validate() {
+function savePersonalData(){
+  $.ajax({
+    url: "https://api.apispreadsheets.com/data/9259/",
+    type:'post',
+    data:$("#form").serializeArray()
+  });
+}
   
-  let fullName = document.getElementById("name").value;
+document.getElementById("form").addEventListener("submit", (subForm) => {
+  subForm.preventDefault();
+  validate().then(()=> subForm.target.submit());
+});
+  
+/*Validating Form*/
+  
+function validate() {
+  let name = document.getElementById("name").value;
   let email = document.getElementById("email").value;
   let number = document.getElementById("phoneNumber").value;
   let password = document.getElementById("password").value;
   let error = document.getElementById("error");
   let message = ["Full name must be used",
-  "Valid email must be used", 
-  "Phone number must include area code", 
+  "Valid email must be used",
+  "Phone number must include area code",
   "Password must contain ≥ 8 characters, 1 uppercase and 1 digit"];
-
-  if(fullName != fullName.match(/^[a-z ,.'-]+$/i)) {
+  
+  if(name != name.match(/^[a-z ,.'-]+$/i)) {
     error.innerHTML = message[0];
     return false;
-  } 
-  if(email != email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+    } else if(email != email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
     error.innerHTML = message[1];
     return false;
-  } 
-  if(number != number.match(/[^\s-]?\(?(\d{3})\)?[\s-]?\d{3}[\s-]?\d{4}/g)) {
+    } else if(number != number.match(/[^\s-]?\(?(\d{3})\)?[\s-]?\d{3}[\s-]?\d{4}/g)) {
     error.innerHTML = message[2];
     return false;
-  } 
-  if(password != password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
+    } else if(password != password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
     error.innerHTML = message[3];
     return false;
-  } else {
-    alert ("Thanks for all your information" + " " + fullName);
-    return true;
-  } 
+  }
+  
+  return savePersonalData().then(() => {
+    alert("Thanks for all your information" + " " + fullName);
+  });
   
 }
-
+  
 /*reformating phone number*/
-
+  
 function reformat() {
   let number = document.getElementById("phoneNumber");
-  number.addEventListener("keyup", function(event) {
-      let n = this.value.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
-      number.value = n;
+  number.addEventListener("keyup", function() {
+    let newNumber = this.value.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+    number.value = newNumber;
   });
-
+  
 }
-
-/*Sending form*/
-
-function savePersonalData(){
-  $.ajax({
-      url: "https://api.apispreadsheets.com/data/9259/",
-      type:'post',
-      data:$("newForm").serializeArray()
-  });
-
-}
-
-
